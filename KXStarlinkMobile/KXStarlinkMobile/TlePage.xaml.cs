aa862a3e3265d671e9421bd3c397e48136efb768
Daniel Siwiec <danielwezut@gmail.com>
2020-10-05 20:35:50 +0200

    Zmiana dla emila

--- KXStarlinkMobile/KXStarlinkMobile/TlePage.xaml.cs
+++ KXStarlinkMobile/KXStarlinkMobile/TlePage.xaml.cs
@@ -33,7 +33,7 @@ namespace KXStarlinkMobile
                     //return ParserTLE.ParseFile(@"starlink.txt");
 
 
-                } sadsadas
+                } //To dla emila
                 catch (WebException webEx)
                 {
                     if (webEx.Status == WebExceptionStatus.ConnectFailure)
